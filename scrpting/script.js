e8fefffb74ad22387da2913c4f184a2505c9dee3
Zakir <zakir.nevsky@gmail.com>
2022-06-29 21:00:19 +0600

    New Update

--- scrpting/script.js
+++ scrpting/script.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 const animItems = document.querySelectorAll("._anim-items");
-
 if (animItems.length > 0) {
   window.addEventListener("scroll", animOnScroll);
   function animOnScroll(params) {
@@ -38,44 +36,3 @@ if (animItems.length > 0) {
     animOnScroll();
   }, 300);
 }
-=======
-const animItems = document.querySelectorAll("._anim-items");
-
-if (animItems.length > 0) {
-  window.addEventListener("scroll", animOnScroll);
-  function animOnScroll(params) {
-    for (let index = 0; index < animItems.length; index++) {
-      const animItem = animItems[index];
-      const animItemHeight = animItem.offsetHeight;
-      const animItemOffset = offset(animItem).top;
-      const animStart = 4;
-
-      let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-      if (animItemHeight > window.innerHeight) {
-        animItemPoint = window.innerHeight - window.innerHeight / animStart;
-      }
-
-      if (
-        pageYOffset > animItemOffset - animItemPoint &&
-        pageYOffset < animItemOffset + animItemHeight
-      ) {
-        animItem.classList.add("_active");
-      } else {
-        if (!animItem.classList.contains("_anim-no-hide")) {
-          animItem.classList.remove("_active");
-        }
-      }
-    }
-  }
-  function offset(el) {
-    const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-  }
-  setTimeout(() => {
-    animOnScroll();
-  }, 300);
-}
->>>>>>> 00f83683cc414e29f01cee740c10d386db85c0f5
